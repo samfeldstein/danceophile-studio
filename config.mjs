bd36430e485111b68e08b217f16e8e493d156b6a
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-11-06 17:13:23 -0700

    Add site skeleton

---
+++ config.mjs
@@ -0,0 +1,11 @@
+export const site = {
+  title: "Postoley Dance Ensemble",
+  description:
+    "Discover the Postoley Ukrainian Dance Ensemble! Join us to learn vibrant Ukrainian dances, celebrate cultural heritage, and connect with a community passionate about Ukrainian traditions. Welcoming dancers of all levels!",
+  url: "https://postoley.netlify.app",
+};
+
+export const dev = {
+  name: "Sam Feldstein",
+  email: "sam@samfeldstein.xyz"
+}
