c5303c6c812255ec84a766876bfcb139cb568dd9
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-11-21 13:32:54 -0600

    Add events page

---
+++ events.mjs
@@ -0,0 +1,14 @@
+export const cottonwood = [{
+  year: 2025,
+  events: [{
+    date: "June 24",
+    dance: "Salsa"
+  }, {
+    date: "July 1",
+    dance: "Salsa"
+  },
+  {
+    date: "July 15",
+    dance: "Cuban Salsa"
+  }]
+}]
