501dcdbf5fe0cd815ef2d49dc91a741d80c44516
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-12-26 14:49:52 -0700

    Add contact page

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 export const site = {
   title: "Danceophile Studio",
   description:
-    "Danceophile Studio in Boulder offers lessons in ballroom, swing, folk, contra, and country dance, led by Colorado Legends of Dance recipient Tom Masterson. Our Postoley Dance Ensemble offers a strong focus on Ukrainian culture through performances and workshops for schools, festivals, and events. Classes are open to all experience levels—no partner needed.",
+    "Danceophile Studio in Boulder offers lessons in ballroom, swing, folk, contra, and country dance, led by Colorado Legends of Dance recipient Tom Masterson. Our Postoley Dance Ensemble offers a strong focus on Ukrainian culture through performances and workshops for schools, festivals, and events. Classes are open to all experience levels. No partner needed.",
   url: "https://danceophile.netlify.app",
 };
 
