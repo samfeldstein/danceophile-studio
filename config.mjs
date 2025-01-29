c851ada975dadcc5cbdb7b23dcbb82b8816e22f8
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-01-29 11:37:24 -0700

    Update base url

--- config.mjs
+++ config.mjs
@@ -2,7 +2,7 @@ export const site = {
   title: "Danceophile Studio",
   description:
     "Danceophile Studio in Boulder offers lessons in ballroom, swing, folk, contra, and country dance, led by Colorado Legends of Dance recipient Tom Masterson. Our Postoley Dance Ensemble offers a strong focus on Ukrainian culture through performances and workshops for schools, festivals, and events. Classes are open to all experience levels. No partner needed.",
-  url: "https://danceophile.netlify.app",
+  url: "https://danceophile.com",
 };
 
 export const dev = {
